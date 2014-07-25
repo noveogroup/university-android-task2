d956eea2ebff619c70ab48c41d362a0160f2bd02
Sergey Bragin <sergeybragin@rocketmail.com>
2014-07-25 17:20:27 +0700

    Implement layouts
    Implement layouts.

--- Task2/src/main/java/com/noveogroup/task2/MainActivity.java
+++ Task2/src/main/java/com/noveogroup/task2/MainActivity.java
@@ -2,14 +2,30 @@ package com.noveogroup.task2;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.info_edit_layout);
+
+        ((TextView) findViewById(R.id.name_text)).setText("Name");
+        ((TextView) findViewById(R.id.surname_text)).setText("Surname");
+        ((TextView) findViewById(R.id.skills_text)).setText("Skills");
+    }
+
+    public void editSkills(View view) {
+
+    }
+
+    public void saveEdited(View view) {
+
+    }
+
+    public void discardEdited(View view) {
+
     }
 }
