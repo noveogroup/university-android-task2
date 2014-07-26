628f50df89d74bbbec5c35e4d9600f5154e92a00
Arseny Skurko <ars.skurko@yandex.ru>
2014-07-27 02:10:45 +0700

    add header to the listview

--- Task2/src/main/java/com/noveogroup/task2/MainActivity.java
+++ Task2/src/main/java/com/noveogroup/task2/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -26,8 +28,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_layout);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         final List<Employee> employees = new ArrayList<Employee>();
         String name;
