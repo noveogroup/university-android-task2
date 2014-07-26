98db63afa7e2ed72fd090153727910666e142b7a
Yuri Chechulin <chechulin.yuri@gmail.com>
2014-07-26 19:30:12 +0700

    create list adapter and employee class

---
+++ Task2/src/main/java/com/noveogroup/task2/Employee.java
@@ -0,0 +1,16 @@
+package com.noveogroup.task2;
+
+/**
+ * Created by Yuri on 26.07.2014.
+ */
+public class Employee {
+    public String fname;
+    public String lname;
+    public String skills;
+
+    public Employee(String fname, String lname, String skills) {
+        this.fname = fname;
+        this.lname = lname;
+        this.skills = skills;
+    }
+}
--- Task2/src/main/java/com/noveogroup/task2/MainActivity.java
+++ Task2/src/main/java/com/noveogroup/task2/MainActivity.java
@@ -1,12 +1,8 @@
 package com.noveogroup.task2;
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,16 +11,18 @@ import java.util.ArrayList;
 public class MainActivity extends ListActivity {
 
     private ArrayList<Employee> employees = new ArrayList<Employee>();
-    View header;
+    private View headerView;
+    private View headerEdit;
+    private boolean isHeaderInit = false;
+    private boolean isEditMode = false;
+    private int currentEmployer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        header = getLayoutInflater().inflate(R.layout.header, null);
-
-        ListView listView = getListView();
-        listView.addHeaderView(header);
+        headerView = getLayoutInflater().inflate(R.layout.header_view, null);
+        headerEdit = getLayoutInflater().inflate(R.layout.header_edit, null);
 
         for (int i = 0; i < 50; i++) {
             employees.add(new Employee("fname" + i, "lname" + i, "skills" + i));
@@ -34,55 +32,62 @@ public class MainActivity extends ListActivity {
         setListAdapter(adapter);
     }
 
+    public void editSkillsButtonOnClick(View v) {
+        ListView listView = getListView();
+        listView.removeHeaderView(headerView);
+        listView.addHeaderView(headerEdit);
+        isEditMode = true;
+        updateHeader();
+    }
+
+    public void saveSkillsButtonOnClick(View v) {
+
+        TextView skills = (TextView) headerEdit.findViewById(R.id.edit_skills_view);
+        employees.get(currentEmployer).skills = skills.getText().toString();
+
+        ListView listView = getListView();
+        listView.removeHeaderView(headerEdit);
+        listView.addHeaderView(headerView);
+        isEditMode = false;
+        updateHeader();
+    }
+
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
+        if (!isHeaderInit) {
+            ListView listView = getListView();
+            listView.addHeaderView(headerView);
+            isHeaderInit = true;
+        } else {
+            position --;
+        }
 
-        if (position > 0) {
+        currentEmployer = position;
 
-            Employee employee = (Employee) this.getListAdapter().getItem(position - 1);
+        updateHeader();
+    }
 
-            TextView fname = (TextView) header.findViewById(R.id.fname);
-            TextView lname = (TextView) header.findViewById(R.id.lname);
-            TextView skills = (TextView) header.findViewById(R.id.skills);
+    private void updateHeader() {
+        Employee employee = (Employee) this.getListAdapter().getItem(currentEmployer);
+
+        if (!isEditMode) {
+            TextView fname = (TextView) headerView.findViewById(R.id.fname);
+            TextView lname = (TextView) headerView.findViewById(R.id.lname);
+            TextView skills = (TextView) headerView.findViewById(R.id.skills);
 
             fname.setText(employee.fname);
             lname.setText(employee.lname);
             skills.setText(employee.skills);
-        }
-    }
+        } else {
+            TextView fname = (TextView) headerEdit.findViewById(R.id.fname);
+            TextView lname = (TextView) headerEdit.findViewById(R.id.lname);
+            TextView skills = (TextView) headerEdit.findViewById(R.id.edit_skills_view);
 
-    public class Employee {
-        public String fname;
-        public String lname;
-        public String skills;
-
-        public Employee(String fname, String lname, String skills) {
-            this.fname = fname;
-            this.lname = lname;
-            this.skills = skills;
-        }
-    }
-
-    public class MySimpleArrayAdapter extends ArrayAdapter<Employee> {
-        private final Context context;
-        private final ArrayList<Employee> values;
-
-        public MySimpleArrayAdapter(Context context, ArrayList<Employee> values) {
-            super(context, R.layout.row_layout, values);
-            this.context = context;
-            this.values = values;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View rowView = inflater.inflate(R.layout.row_layout, parent, false);
-            TextView textView = (TextView) rowView.findViewById(R.id.label);
-            textView.setText(values.get(position).fname + " " + values.get(position).lname);
-
-            return rowView;
+            fname.setText(employee.fname);
+            lname.setText(employee.lname);
+            skills.setText(employee.skills);
         }
     }
 }
