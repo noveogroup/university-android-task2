2b4c5b5d7018f8a6f34e7e4177f63c6c2195c784
Arseny Skurko <ars.skurko@yandex.ru>
2014-07-27 23:18:02 +0700

    everything is done but landscape orientation

--- Task2/src/main/java/com/noveogroup/task2/MainActivity.java
+++ Task2/src/main/java/com/noveogroup/task2/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,24 +27,30 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
 
+    private int currentEmployeeNum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button editButton = (Button)findViewById(R.id.editButton);
+        final Button saveButton = (Button)findViewById(R.id.saveButton);
+        final EditText skillsEditor = (EditText)findViewById(R.id.skillsEditor);
         final TextView nameTextView = (TextView)findViewById(R.id.name);
         final TextView surnameTextView = (TextView)findViewById(R.id.surname);
         final TextView skillsTextView = (TextView)findViewById(R.id.skills);
 
+        editButton.setVisibility(View.GONE);
+        saveButton.setVisibility(View.GONE);
+        skillsEditor.setVisibility(View.GONE);
+
         final List<Employee> employees = new ArrayList<Employee>();
-        String name;
-        String surname;
-        ArrayList<String> skills = new ArrayList<String>();
         Random rnd = new Random();
 
         for (int i = 0; i < 100; i++) {
-            name = StringGenerator.generateString(8);
-            surname = StringGenerator.generateString(8);
+            String name = StringGenerator.generateString(8);
+            String surname = StringGenerator.generateString(8);
             int max = rnd.nextInt(5) + 1;
             String newSkills = "";
             for (int j = 0; j < max; j++) {
@@ -52,17 +60,44 @@ public class MainActivity extends Activity {
                 }
             }
             employees.add(new Employee(name, surname, newSkills));
-            skills.clear();
         }
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skillsEditor.setText(skillsTextView.getText());
+                skillsTextView.setVisibility(View.GONE);
+                skillsEditor.setVisibility(View.VISIBLE);
+                editButton.setVisibility(View.GONE);
+                saveButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                employees.set(currentEmployeeNum, new Employee(nameTextView.getText().toString(), surnameTextView.getText().toString(), skillsEditor.getText().toString()));
+                saveButton.setVisibility(View.GONE);
+                editButton.setVisibility(View.VISIBLE);
+                skillsEditor.setVisibility(View.GONE);
+                skillsTextView.setVisibility(View.VISIBLE);
+                skillsTextView.setText(skillsEditor.getText());
+            }
+        });
 
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(new MyAdapter(this, employees));
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                currentEmployeeNum = position;
                 nameTextView.setText(employees.get(position).getName());
                 surnameTextView.setText(employees.get(position).getSurname());
                 skillsTextView.setText(employees.get(position).getSkills());
+                editButton.setVisibility(View.VISIBLE);
+                saveButton.setVisibility(View.GONE);
+                skillsEditor.setVisibility(View.GONE);
+                skillsTextView.setVisibility(View.VISIBLE);
             }
         });
     }
